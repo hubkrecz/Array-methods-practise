724972c9cae0d5f413c4d34747716eda5a54a0ab
hubkrecz <107886221+hubkrecz@users.noreply.github.com>
2022-07-08 18:51:55 +0200

    cwicznia

---
+++ src/script.js
@@ -0,0 +1,20 @@
+let amongus = [1, 2, 4, 3, 8, 1, 1, 3, 8, 9, 3, 4, 5, 1, 9]
+const getStringFromArray = (array) => {
+    devider = array.toString();
+    console.log(devider);
+}
+const getSortedConcatedArray = (array) => {
+    soertedArray = array.sort((a, b) => a - b);
+    console.log(soertedArray);
+}
+const getFlattenArray = (array) => {
+    array = array.flat();
+    console.log(array);
+    return array;
+}
+const getUniqueElements = (array) => {
+    const newArray = array.map((elment) => {
+
+    })
+}
+console.log(getUniqueElements(amongus))
